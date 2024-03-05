315a562081390c8c16e87424a4a20a99bd5455e5
peiyi <peiyi523@gmail.com>
2024-03-05 23:51:28 +0800

    在js中將日期的函式寫好

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 const amountEl = document.querySelector("#amount");
 const exchangeEl = document.querySelector("#exchange");
 const baseEl = document.querySelector("#base");
-const convertEl = document.querySelector("#convert")
+const convertEl = document.querySelector("#convert");
+
 
 console.log(convertEl, amountEl, exchangeEl, baseEl);
 
@@ -13,5 +14,29 @@ function convertExchange() {
     let base = baseEl.value;
 
     console.log(amount, exchange, base);
+}
+
+
+
+function getCurrentTime() {
+    let currentDate = new Date();
+    let year = currentDate.getFullYear();
+    let month = currentDate.getMonth() + 1;
+    let day = currentDate.getDate();
+    let hours = currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let currentTimeStr = year + "年" + month + "月" + day + "日" + " " + hours + ":" + minutes;
+    return currentTimeStr;
 
 }
+
+function updateClock() {
+    let timeEl = document.getElementById("time");
+    if (timeEl) {
+        timeEl.innerHTML = getCurrentTime();
+    }
+}
+
+updateClock();
+
+// 2024年3月4日 22:53
