73ca31964a39c62f878128b705386d9d20f6d1d7
peiyi <peiyi523@gmail.com>
2024-03-10 15:44:53 +0800

    呈現轉換後的資料

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ function convertExchange() {
     console.log(amount, exchange, base);
     document.querySelector(".examount").innerHTML = amount;
     document.querySelector(".exchange").innerHTML = exchange;
-    document.querySelector(".exbase").innerHTML = getRate()
+
     document.querySelector(".base").innerHTML = base;
     const resultEl = document.querySelector("#result");
     resultEl.style.display = "none";
@@ -37,6 +37,7 @@ function convertExchange() {
         return;
     }
 }
+
 resetEl.addEventListener("click", resetForm);
 function resetForm() {
     amountEl.value = "";
@@ -44,62 +45,26 @@ function resetForm() {
     baseEl.value = "";
 }
 
-// exbase的python函式
-// function get_rate() {
-//     let url = "https://rate.bot.com.tw/xrt/flcsv/0/day";
-//     let df = pd.read_csv(url).iloc[:, [2, 12]];
-//     let new_columns = { "": "幣別", "現金": "即期買入", "現金.1": "即期賣出" };
-//     let df = df.rename(columns = new_columns);
-//     let df["原幣兌台幣"] = df.mean(axis = 1);
-//     // 先指定好要操作的欄位名稱
-//     let cal_column = "原幣兌台幣";
-//     // 找到指定操作欄位的第一個數字
-//     let first_value = df[cal_column].iloc[0];
-//     // 將要操作的那欄每一個數字除以第一個數字，並將結果放在新的一欄中
-//     let df["原幣兌美元"] = df[cal_column] / first_value;
-//     df_list = df.values.tolist();
-//     console.log(datas);
-//     return df_list;
-
-
-
+const exbaseEl = document.querySelector(".amount")
+exbaseEl.addEventListener("input", exbaseForm);
+const rateData = {};
 async function getRate() {
-    const url = "https://rate.bot.com.tw/xrt/flcsv/0/day";
-    const response = await fetch(url);
-    const data = await response.text();
+    const res = await fetch(`https://api.exchangerate-api.com/v4/latest/USD`);
+    const data = await res.json();
+    // rateData["CNY"] = data.rates["CNY"];
+    console.log(data);
 
-    // Convert CSV to JSON
-    const jsonData = Papa.parse(data, { header: true });
-    const currencies = jsonData.data.map(entry => ({
-        '幣別': entry[''],
-        '即期買入': parseFloat(entry['現金']),
-        '即期賣出': parseFloat(entry['現金.1'])
-    }));
+}
 
-    // Calculate original currency to TWD exchange rate
-    const firstValue = currencies[0]['原幣兌台幣'];
-    currencies.forEach(currency => {
-        currency['原幣兌美元'] = currency['原幣兌台幣'] / firstValue;
-    });
-
-    // Extract relevant data
-    const newData = currencies.map(currency => ({ '原幣兌美元': currency['原幣兌美元'] }));
-
-    // Convert JSON to string
-    const jsonString = JSON.stringify(newData);
-    return jsonString;
+function exbaseForm() {
+    const exchangeRate = rateData[exchangeEl.value];
+    if (exchangeRate !== undefined) {
+        exbaseEl.value = (Number(this.value) * Number(rateData[exchangeEl.value])).toFixed(2);
+    }
 }
 
 
-
-
-
-
-
-
-
-
-
+getRate();
 
 
 
